53d343d7e011086d667d044cf21812a3065714af
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-07-19 11:18:31 +0700

    setup appjs

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const model = require('./models');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', function(req,res){
+  res.send('hai')
+})
+
+
+app.listen(3002);
